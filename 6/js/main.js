367699f2a76e1030048f764fce979f0557366435
Keks <keks@htmlacademy.ru>
2022-07-09 22:10:48 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,5 @@
+import {createPhotos} from './data.js';
+import './picture.js';
+import './big-picture.js';
+
+createPhotos();
---
+++ 6/js/picture.js
@@ -0,0 +1,23 @@
+import { createPhotos } from './data.js';
+import {openBigPicture} from './big-picture.js';
+
+const listPhotos = document.querySelector('.pictures');
+const fragmentPhotos = document.createDocumentFragment();
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const generatedPhotos = createPhotos();
+
+generatedPhotos.forEach((generatedPhoto) => {
+  const photoElement = pictureTemplate.cloneNode(true);
+  photoElement.querySelector('.picture__img').src =  generatedPhoto.url;
+  photoElement.querySelector('.picture__likes').textContent = generatedPhoto.likes;
+  photoElement.querySelector('.picture__comments').textContent = generatedPhoto.comments.length;
+
+  photoElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openBigPicture(generatedPhoto);
+  });
+
+  fragmentPhotos.appendChild(photoElement);
+});
+
+listPhotos.appendChild(fragmentPhotos);
