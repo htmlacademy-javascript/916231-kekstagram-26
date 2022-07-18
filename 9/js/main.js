55f2ad03d078738d0a1fac006d6b4258a1fc37f5
Keks <keks@htmlacademy.ru>
2022-07-18 18:32:09 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,8 @@
+import {createPhotos} from './data.js';
+import './picture.js';
+import './big-picture.js';
+import './user-form.js';
+import './scale-image.js';
+import './filter-image.js';
+
+createPhotos();
