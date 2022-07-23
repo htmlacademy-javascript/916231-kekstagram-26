ad15070f0147f4aadd0fd5d8b421adcfcdea45e7
Keks <keks@htmlacademy.ru>
2022-07-23 17:59:10 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/filter-image.js
@@ -0,0 +1,163 @@
+const imagePreviewElement = document.querySelector('.img-upload__preview')
+  .children[0];
+const sliderElement = document.querySelector('.effect-level__slider');
+const effectValueElement = document.querySelector('.effect-level__value');
+const filtersElement = document.querySelector('.effects__list');
+const sliderBlockElement = document.querySelector('.img-upload__effect-level');
+
+const filtersOptions = {
+  'effect-chrome': {
+    min: 0,
+    max: 1,
+    step: 0.1,
+    classImage: 'effects__preview--chrome',
+  },
+  'effect-sepia': {
+    min: 0,
+    max: 1,
+    step: 0.1,
+    classImage: 'effects__preview--sepia',
+  },
+  'effect-marvin': {
+    min: 0,
+    max: 100,
+    step: 1,
+    classImage: 'effects__preview--marvin',
+  },
+  'effect-phobos': {
+    min: 0,
+    max: 3,
+    step: 0.1,
+    classImage: 'effects__preview--phobos',
+  },
+  'effect-heat': {
+    min: 1,
+    max: 3,
+    step: 0.1,
+    classImage: 'effects__preview--heat',
+  },
+};
+
+let currentFilter = 'effect-none';
+
+const removeImageClass = () => {
+  imagePreviewElement.className = '';
+};
+
+const hiddenSlider = () => {
+  sliderBlockElement.classList.add('hidden');
+};
+
+const showSlider = () => {
+  sliderBlockElement.classList.remove('hidden');
+};
+
+const updateOptions = (options) => {
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: options.min,
+      max: options.max,
+    },
+    start: options.max,
+    step: options.step,
+    format: {
+      to: function (value) {
+        if (Number.isInteger(value)) {
+          return value.toFixed(0);
+        }
+        return value.toFixed(1);
+      },
+      from: function (value) {
+        return value;
+      },
+    },
+  });
+};
+
+
+hiddenSlider();
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+});
+const Filters = {
+  EFFECT_NONE: 'effect-none',
+  EFFECT_CHROME: 'effect-chrome',
+  EFFECT_SEPIA: 'effect-sepia',
+  EFFECT_MARVIN: 'effect-marvin',
+  EFFECT_PHOBOS: 'effect-phobos',
+  EFFECT_HEAT: 'effect-heat',
+};
+
+const filterFunctions = {
+  setFilterNone: () => {
+    imagePreviewElement.style.filter = null;
+  },
+  setFilterChrome: () => {
+    imagePreviewElement.style.filter = `grayscale(${effectValueElement.value})`;
+  },
+  setFilterSepia: () => {
+    imagePreviewElement.style.filter = `sepia(${effectValueElement.value})`;
+  },
+  setFilterMarvin: () => {
+    imagePreviewElement.style.filter = `invert(${effectValueElement.value}%)`;
+  },
+  setFilterPhobos: () => {
+    imagePreviewElement.style.filter = `blur(${effectValueElement.value}px)`;
+  },
+  setFilterHeat: () => {
+    imagePreviewElement.style.filter = `brightness(${effectValueElement.value})`;
+  },
+};
+
+const removeFilter = () => {
+  currentFilter = 'effect-none';
+  filterFunctions.setFilterNone();
+  removeImageClass();
+  hiddenSlider();
+};
+
+sliderElement.noUiSlider.on('update', () => {
+  effectValueElement.value = sliderElement.noUiSlider.get();
+  switch (currentFilter){
+    case Filters.EFFECT_NONE:
+      filterFunctions.setFilterNone();
+      break;
+    case Filters.EFFECT_CHROME:
+      filterFunctions.setFilterChrome();
+      break;
+    case Filters.EFFECT_SEPIA:
+      filterFunctions.setFilterSepia();
+      break;
+    case Filters.EFFECT_MARVIN:
+      filterFunctions.setFilterMarvin();
+      break;
+    case Filters.EFFECT_PHOBOS:
+      filterFunctions.setFilterPhobos();
+      break;
+    case Filters.EFFECT_HEAT:
+      filterFunctions.setFilterHeat();
+      break;
+  }
+});
+
+filtersElement.addEventListener('change', (evt) => {
+  currentFilter = evt.target.id;
+  effectValueElement.value = evt.target.id;
+  if (evt.target.id === 'effect-none') {
+    removeFilter();
+  } else {
+    showSlider();
+    updateOptions(filtersOptions[evt.target.id]);
+    removeImageClass();
+    imagePreviewElement.classList.add(filtersOptions[evt.target.id].classImage);
+  }
+});
+
+export {removeFilter};
---
+++ 12/js/filter-posts.js
@@ -0,0 +1,44 @@
+import {getRandomArrayUniqueNumbers, debounce} from './util.js';
+import {createPictures} from './picture.js';
+const RANDOM_PICTURE_COUNT = 10;
+
+const filterSectionElement = document.querySelector('.img-filters');
+const formFilterElement = filterSectionElement.querySelector('.img-filters__form');
+const filterDefault = formFilterElement.querySelector('#filter-default');
+const filterRandom = formFilterElement.querySelector('#filter-random');
+const filterDiscussed = formFilterElement.querySelector('#filter-discussed');
+
+let currentFilter = filterDefault;
+
+const getRandomPictures = (pictures) => {
+  const randomUniqueNumbers = getRandomArrayUniqueNumbers(pictures.length);
+  const randomPictures = [];
+  randomUniqueNumbers.slice(0, RANDOM_PICTURE_COUNT).forEach((randomNumber) => {
+    randomPictures.push(pictures[randomNumber]);
+  });
+  return randomPictures;
+};
+
+const compareCommentsLength = (pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length;
+
+const getFilteredPictures = (pictures) => {
+  switch (currentFilter){
+    case filterDefault:
+      return pictures;
+    case filterRandom:
+      return getRandomPictures(pictures);
+    case filterDiscussed:
+      return pictures.slice().sort(compareCommentsLength);
+  }
+};
+
+filterSectionElement.classList.remove('img-filters--inactive');
+
+formFilterElement.addEventListener('click', debounce((evt) => {
+  currentFilter.classList.remove('img-filters__button--active');
+  currentFilter = evt.target;
+  currentFilter.classList.add('img-filters__button--active');
+  createPictures();
+}));
+
+export {getFilteredPictures};
