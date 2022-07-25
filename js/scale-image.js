51cf0b46630102240eb4dacd87fda2348dc68220
Anna <lilimp8@gmail.com>
2022-07-25 22:51:45 +0600

    Исправляет правки по защите

--- js/scale-image.js
+++ js/scale-image.js
@@ -3,7 +3,7 @@ const MAX_SCALE_VALUE = 1;
 const MIN_SCALE_VALUE = 0.25;
 const SCALE_COEFFICIENT = 100;
 
-const imagePreviewElement = document.querySelector('.img-upload__preview');
+const imagePreviewElement = document.querySelector('.img-upload__preview').children[0];
 const scaleValueElement = document.querySelector('.scale__control--value');
 const smallerScaleElement = document.querySelector('.scale__control--smaller');
 const biggerScaleElement = document.querySelector('.scale__control--bigger');
